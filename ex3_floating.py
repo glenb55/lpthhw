6a019d5ffc8ea0bd3243907cc593d433906afd36
glenb55 <45375622+glenb55@users.noreply.github.com>
2019-08-26 10:51:52 -0700

    Add files via upload

---
+++ ex3_floating.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens.")
+
+print("Hens", 25.0 + 30.0 /6.0)   # order of operation 30/6 = 5 plus 25 = 30
+print ("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Now I will count the eggs")
+
+print( 3.0 + 2.0 + 1.0 -5.0 + 4.0 % 2.0 -1.0 / 4.0 + 6.0)
+
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+print (3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print ("What is 5 -7?" , 5.0 - 7.0 )
+
+print("Oh, thats why its False.")
+
+print("How about some more.")
+
+print ("is it greater?" , 5.0 > -2.0)
+print ("Is it greater than or equal?" , 5.0 >= -2.0)
+print ("is it less than or equal?" , 5.0 <= -2.0)
